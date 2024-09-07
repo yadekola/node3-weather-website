4a2d213496a369db09cdd6f4f8b09b47b273129a
yadekola <yakubadekolaojo059@gmail.com>
2024-09-07 16:56:26 -0700

    Add high and low to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,5 +1,12 @@
 const request = require('request')
 
+// Goal: Add  new data to forecast
+// 
+// 1. Update the forecast string to include new data
+// 2. Commit your changes
+// 3. Push your changes to Github and deploy to Heroku
+// 4. Test your work in the live application!
+
 const forecast = (latitude, longitude, callback) => {
     const url = 'http://api.weatherstack.com/current?access_key=82500b2b13bd487418f4b98bdbba43c8&query=' + latitude + ',' + longitude + '&units=f'
 
@@ -12,7 +19,7 @@ const forecast = (latitude, longitude, callback) => {
             callback('Unable to find location', undefined)
         } else {
             // Success: Pass the formatted forecast string
-            callback(undefined, body.current.weather_descriptions[0] +'. It is currently ' + body.current.temperature + ' degress out. There is a ' + body.current.feelslike + '% chance of rain')
+            callback(undefined, body.current.weather_descriptions[0] +'. It is currently ' + body.current.temperature + ' degress out.  It feels like ' + body.current.feelslike + " degress out. The humidity is " + body.current.humidity + '%.')
             // callback(undefined, body.current.weather_descriptions[0] +'. It is currently' + body.current.temperature + " degress out. It feels like " + body.current.feelslike + ' degress out.')
         }
     })
